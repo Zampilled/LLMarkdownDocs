1dcc924117df6e5dccad754b85050ec519effe21
zamo <alex@karanagai.com>
2025-03-11 11:33:53 +0000

    Updating main and config

--- code/config.py
+++ code/config.py
@@ -3,6 +3,13 @@ from typing import List
 from pydantic import BaseModel
 
 
+"""
+LLM response structure
+This is the structure to be used to dictate how the markdown file is generated.
+Fields are inferred through the name and type of the field.
+Any field named "Name" will be replaced in the markdown with its value.
+Any field named "Code" or "ExampleUse" will be surrounded by a code block in the markdown.
+"""
 class Parameter(BaseModel):
     Name: str
     Type: str
@@ -25,14 +32,16 @@ class NPMExport(BaseModel):
     ExampleUse: str
     TestCases: List[TestCase]
 
+"""
+----------------------------------------------------
+"""
 
-ollama_model: str = "llama3.2"
-
+#The prompt to be used by the model
 my_prompt: str = """
 You are an expert NodeJS export documentation writer. 
 For each export you create a description, document parameters, document the return value, 
 create and ExampleUse to be used as an example of the export, and some relevant TestCases that can 
-be used to test the exports in a meaningful way. IMPORTANT make sure all code written for the ExampleUse and
+be used to test.sh the exports in a meaningful way. IMPORTANT make sure all code written for the ExampleUse and
 TestCases compiles and is written as a js or jsx file.
 Your job is to take the method immediately proceeding this sentence and 
 use the codebase after it to create extensive documentation for that export.
--- code/main.py
+++ code/main.py
@@ -1,30 +1,37 @@
 import argparse
 from tqdm import tqdm
 
-from llm_proccessing.createMarkdown import createMarkdown
-from llm_proccessing.getFiles import getFiles
-from llm_proccessing.getLLMResponse import getLLMResponse
+
+from llm_proccessing.createMarkdown import create_markdown
+from llm_proccessing.getFiles import get_files
+from llm_proccessing.getLLMResponse import get_llm_response
 
 from ast import literal_eval
 
-def main(package,methods, outputdir):
-    package = "@rescui/use-glow-hover"
-    methods = ["glowHoverEffect", "useGlowHover"]
-    codebase = getFiles(package)
+def main(package,exports, outputdir, model):
+    """
+    Main function for creating markdown docs for a npm package using the exports
+    :param package: name of package to be documented
+    :param exports: array of exports to be documented
+    :param outputdir: output directory for the markdown files
+    :param model: name of the model to be used
+    :return: void
+    """
+    codebase = get_files(package)
     data = []
-    for method in tqdm(methods):
+    for method in tqdm(exports):
         tqdm.write(method+" export being doccumented")
-        response = getLLMResponse(method,codebase).model_dump_json()
+        response = get_llm_response(method, codebase, model).model_dump_json()
         response_dict = {"Name": method}
         response_dict.update(literal_eval(response))
         data.append(response_dict)
-    createMarkdown(package,data, outputdir)
+    create_markdown(package, data, outputdir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-p","--package", help="The package name")
     parser.add_argument("-o","--outputdir", help="The markdown output directory")
-    parser.add_argument("-m","--methods", help="The method names")
-
+    parser.add_argument("-m","--model", help="model to be used")
+    parser.add_argument("-e","--export", help="The export names seperated by space")
     args = parser.parse_args()
-    main(args.package,args.methods, args.outputdir )#.split(" "))
+    main(args.package,args.export.split(" "), args.outputdir, args.model)
