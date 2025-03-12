3957208b116c6faf9898e31e4231a9b3788c2284
zamo <alex@karanagai.com>
2025-03-12 18:28:36 +0000

    Creating Start script

--- code/start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Did this instead of directly grabbing env variables incase
-# script wants to be run independently of docker.
-python3 main.py -p "$PACKAGENAME" -e "$PACKAGEEXPORTS" -o "output/$PACKAGENAME.md" -m "$MODELNAME"
---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+##################
+##   Config     ##
+##################
+
+PACKAGE_NAME="@rescui/use-glow-hover"
+PACKAGE_EXPORTS="useGlowHover GlowHoverHookOptions glowHoverEffect GlowHoverOptions"
+MODEL_NAME="tinyllama"
+TYPESCRIPT=true
+
+########################
+
+cd code || return
+
+echo "Pulling Ollama Model"
+ollama pull "$MODEL_NAME"
+
+npm i "$PACKAGE_NAME"
+
+# Did this instead of directly grabbing env variables incase
+# script wants to be run independently of docker.
+python3 main.py -p "$PACKAGE_NAME" -e "$PACKAGE_EXPORTS" -o "output/output.md" -m "$MODEL_NAME" -t $TYPESCRIPT
